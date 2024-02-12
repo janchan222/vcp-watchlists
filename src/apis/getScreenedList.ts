9ea44a2ac9cd2cc0ccb936c3b0eb94bd41714b97
Michael Chang <m.chihochang@gmail.com>
2024-02-13 01:28:17 +0800

    finished

---
+++ src/apis/getScreenedList.ts
@@ -0,0 +1,3 @@
+export const getScreenedList = async () => {
+  return;
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"></meta>
     </html>
   );
 }
